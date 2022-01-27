8676c2db37a8eb7d78dd74fa5ac822ba54c79cfd
sdfmv <98357247+sdfmv@users.noreply.github.com>
2022-01-27 13:34:05 +0300

    v1

---
+++ src/pages/admin/VehiclesNewPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import VehicleNew from "../../components/admin/VehicleNew";
+import PageHeader from "../../components/common/PageHeader";
+import Spacer from "../../components/common/Spacer";
+
+const VehiclesNewPage = () => {
+  return (
+    <>
+      <PageHeader title="Vehicle Management" />
+      <Spacer />
+      <Container>
+        <VehicleNew />
+      </Container>
+      <Spacer />
+    </>
+  );
+};
+
+export default VehiclesNewPage;
